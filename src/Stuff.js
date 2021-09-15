dcae5c9efb571861390a620bf06649e994c1e64d
tawe-segment <88668535+tawe-segment@users.noreply.github.com>
2021-09-15 15:53:27 +0100

    Publish ReactApp to Github

---
+++ src/Stuff.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  componentDidMount() {
+    window.analytics.page('Stuff');
+  }
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Mauris sem velit, vehicula eget sodales vitae,
+        rhoncus eget sapien:</p>
+        <ol>
+          <li>Nulla pulvinar diam</li>
+          <li>Facilisis bibendum</li>
+          <li>Vestibulum vulputate</li>
+          <li>Eget erat</li>
+          <li>Id porttitor</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
